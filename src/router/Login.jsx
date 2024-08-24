7f1b867ce8bd08a470e3feeff33cf7b4acb3c1e5
MinWook6457 <minuk6457@gmail.com>
2024-08-24 20:55:26 +0900

    init : CRA 및 라우터 추가

---
+++ src/router/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Login = (props) => {
+	return (
+		<>
+			<h3>로그인</h3>
+		</>
+	);
+};
+
+export default Login;
---
+++ src/router/mainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const mainPage = (props) => {
+	return (
+		<>
+			<h3>안녕하세요. 메인페이지 입니다.</h3>
+		</>
+	);
+};
+
+export default mainPage;
