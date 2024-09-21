1114d4c3242d198244ea92157f8b5d883caaa3f4
MinWook6457 <minuk6457@gmail.com>
2024-09-21 21:48:21 +0900

    [기능수정] : 홈 캐러셀 이미지 수정

--- src/pages/components/CarouselFadeExample.jsx
+++ src/pages/components/CarouselFadeExample.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import ImageImport from './ImageImport';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import test from '../../images/test.png';
+import home from '../../images/HOME.png';
 import study from '../../images/study.png';
 
 function CarouselFadeExample() {
@@ -10,7 +10,7 @@ function CarouselFadeExample() {
     <Carousel fade>
       <Carousel.Item>
          <a href="https://www.notion.so/Study-With-Me-7ffb049d2bba4814a4da7dc010d8216e">
-            <ImageImport src={test} alt="인프런" />
+            <ImageImport src={home} alt="인프런" />
           </a>
         <Carousel.Caption>
           <h3>Welcome</h3>
@@ -19,15 +19,11 @@ function CarouselFadeExample() {
       <Carousel.Item>
          <ImageImport src={study} alt="스터디" />
       <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-          <ImageImport src={test} alt="인프런" />
+          <ImageImport src={home} alt="인프런" />
       <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
