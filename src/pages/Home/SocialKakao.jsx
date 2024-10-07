ad3ff997dcc95826c72b1181a4516ce87af9073c
MinWook6457 <minuk6457@gmail.com>
2024-10-07 22:27:16 +0900

    [기능수정] : 카카오 리다이렉트 URI 수정

--- src/pages/Home/SocialKakao.jsx
+++ src/pages/Home/SocialKakao.jsx
@@ -2,9 +2,9 @@
 const SocialKakao = ()=>
 	{
 		const REST_API_KEY = process.env.REACT_APP_REST_API_KEY //REST API KEY
-		const REDIRECT_URI = 'http://localhost:3000/kakao/callback' //Redirect URI
-		const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-
+		const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI //Redirect URI
+		
+		const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`	
 		window.location.href = kakaoURL;
 	}
 
